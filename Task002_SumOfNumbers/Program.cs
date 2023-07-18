c71dfc4fd3d39938f730b295d7f35218a985c34f
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-07-19 06:22:54 +1000

    Task 002 Sum Of Numbers

---
+++ Task002_SumOfNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = M; i <= N; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine("M = {0}; N = {1} -> {2}", M, N, sum);
