e290f2342cc4936ceb2fd251fbede6483a16ee0e
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-07-21 01:01:36 +1000

    Task 003 Ackerman

---
+++ Task003_Ackerman/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using System;
+
+class MainClass
+{
+    public static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите значение m: ");
+        int m = Int32.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение n: ");
+        int n = Int32.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+        Console.WriteLine("m = " + m + ", n = " + n + " -> A(m,n) = " + result);
+    }
+}
